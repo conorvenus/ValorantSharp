82e5f259767e40b981e51fa098085ebeda3c62a7
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2021-09-30 21:18:27 +0200

    fix formatting

--- ValorantSharp/Objects/Game/ValorantPresence.cs
+++ ValorantSharp/Objects/Game/ValorantPresence.cs
@@ -1,7 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ValorantSharp.Objects.Game
 {
@@ -38,9 +35,9 @@ namespace ValorantSharp.Objects.Game
 		public string queueEntryTime { get; set; } = "0001.01.01-00.00.00";
 		public string playerCardId { get; set; } = "c89194bd-4710-b54e-8d6c-60be6274fbb2";
 		public string playerTitleId { get; set; } = "566b6a77-4f72-af35-6d17-43be14e73cb7";
-        public string preferredLevelBorderId { get; set; } = "";
+		public string preferredLevelBorderId { get; set; } = "";
 		public int accountLevel { get; set; } = 1;
-        public int competitiveTier { get; set; } = 24;
+		public int competitiveTier { get; set; } = 0;
 		public int leaderboardPosition { get; set; } = 0;
 		public bool isIdle { get; set; } = false;
 	}
